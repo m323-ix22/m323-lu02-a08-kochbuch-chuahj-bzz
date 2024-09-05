3247948931787b63a60110e0936c9d314fa52a6c
chuaj <chuahj@bzz.ch>
2024-09-05 10:07:16 +0200

    done

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ import json
 # Dein Code kommt hier hin
 
 def adjust_recipe(full_recipe, num_servings):
-    factor_serving = num_servings / full_recipe["servings"]
+    factor_serving = num_servings / full_recipe['servings']
     adjusted_amount = {}
-    for ingredient, amount in full_recipe["ingredients"].items():
+    for ingredient, amount in full_recipe['ingredients'].items():
         adjusted_amount[ingredient] = amount * factor_serving
-    adjusted_recipe = {
-        "title": full_recipe["title"],
-        "ingredients": adjusted_amount,
-        "servings": num_servings
+    new_recipe = {
+        'title': full_recipe['title'],
+        'ingredients': adjusted_amount,
+        'servings': num_servings
     }
-    return adjusted_recipe
+    return new_recipe
 
 
 def load_recipe(new_json):
@@ -25,7 +25,8 @@ def load_recipe(new_json):
 
 if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, ' \
+                  '"Minced Meat": 500}, "servings": 4} '
     # Dein Code kommt hier hin
     original_recipe = load_recipe(recipe_json)
 
